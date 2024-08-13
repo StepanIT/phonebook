63dc9456da8c252a8433d4e54a57b6fd4ffcd619
Степан <19stepan94@mail.ru>
2024-08-13 15:19:34 +0500

    commit

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -1,4 +1,26 @@
 'use strict';
+const data = [
+  {
+    name: 'Иван',
+    surname: 'Петров',
+    phone: '+79514545454',
+  },
+  {
+    name: 'Игорь',
+    surname: 'Семёнов',
+    phone: '+79999999999',
+  },
+  {
+    name: 'Семён',
+    surname: 'Иванов',
+    phone: '+79800252525',
+  },
+  {
+    name: 'Мария',
+    surname: 'Попова',
+    phone: '+79876543210',
+  },
+];
 
 {
   const createContainer = () => {
@@ -58,8 +80,80 @@
     };
   };
 
-  const init = (selectorApp, title) => {
-    const app = document.querySelector(selectorApp);
+  const createTable = () => {
+    const table = document.createElement('table');
+
+    table.classList.add('table', 'table-striped');
+
+    const thead = document.createElement('thead');
+    thead.insertAdjacentHTML('beforeend', `
+      <tr>
+        <th class="delete">Удалить</th>
+        <th>Имя</th>
+        <th>Фамилия</th>
+        <th>Телефон</th>
+      </tr>
+      `);
+
+      const tbody = document.createElement('tbody');
+
+      table.append(thead, tbody);
+      table.tbody = tbody;
+
+    return table;
+  };
+
+  const createForm = () => {
+    const overlay = document.createElement('div');
+    overlay.classList.add('form-overlay');
+
+    const form = document.createElement('form');
+    form.classList.add('form');
+    form.insertAdjacentHTML('beforeend', `
+      <button class="close" type="button"></button>
+      <h2 class="form-title">Добавить контакт</h2>
+      <div class="form-group">
+        <label class="form-label" for="name">Имя:</label>
+        <input class="form-input" name="name" id="name" type="text" required>
+      </div>
+      <div class="form-group">
+        <label class="form-label" for="surname">Фамилия:</label>
+        <input class="form-input" name="surname"
+        id="surname" type="text" required>
+      </div>
+      <div class="form-group">
+        <label class="form-label" for="phone">Телефон:</label>
+        <input class="form-input" name="phone"
+        id="phone" type="number" required>
+      </div>
+      `);
+
+      const buttonGroup = createButtonsGroup([
+        {
+          className: 'btn btn-primary mr-3',
+          type: 'submit',
+          text: 'Добавить',
+        },
+        {
+          className: 'btn btn-danger',
+          type: 'reset',
+          text: 'Отмена',
+        },
+      ]);
+
+      form.append(...buttonGroup.btns);
+
+      overlay.append(form);
+
+      return {
+        overlay,
+        form,
+      };
+  };
+  
+
+
+  const renderPhoneBook = (app, title) => {
     const header = createHeader();
     const logo = createLogo(title);
     const main = createMain();
@@ -76,9 +170,62 @@
       },
     ]);
 
+    const table = createTable();
+    const form = createForm();
+
     header.headerContainer.append(logo);
-    main.mainContainer.append(buttonGroup.btnWrapper);
+    main.mainContainer.append(buttonGroup.btnWrapper, table, form.overlay);
     app.append(header, main);
+
+    return {
+      list: table.tbody,
+    }
   };
+
+  const createRow = ({name: firstName, surname, phone}) => {
+    console.log('dataObj: ', dataObj);
+
+    const tr = document.createElement('tr');
+
+    const tdDel = document.createElement('td');
+    tdDel.classList.add('delete');
+    const buttonDel = document.createElement('button');
+    buttonDel.classList.add('del-icon')
+    tdDel.append(buttonDel);
+
+    const tdName = document.createElement('td');
+    tdName.textContent = firstName;
+
+    const tdSurname = document.createElement('td');
+    tdSurname.textContent = surname;
+
+    const tdPhone = document.createElement('td');
+    const phoneLink = document.createElement('a');
+    phoneLink.href = `tel:${phone}`;
+    tdPhone.textContent = phone;
+
+    tdPhone.append(phoneLink);
+
+    tr.append(tdDel, tdName, tdSurname, tdPhone);
+
+    return tr;
+
+  }
+
+  const renderContacts = (elem, data) => {
+    const allRow = data.map(createRow);
+    elem.append(...allRow);
+  }
+
+  const init = (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+    const phoneBook = renderPhoneBook(app, title);
+
+    const {list} = phoneBook;
+
+    renderContacts(list, data);
+    // Функционал
+  }
+
   window.phoneBookInit = init;
 }
